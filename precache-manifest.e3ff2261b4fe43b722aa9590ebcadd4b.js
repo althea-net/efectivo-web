1af10c6c193730d5f37c0a1270c2ab394221ccb4
Jehan <jehan.tremback@gmail.com>
2019-01-29 11:27:37 -0800

    Updates

---
+++ precache-manifest.e3ff2261b4fe43b722aa9590ebcadd4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b15a8c6575231a6683cc8982faab6eb",
+    "url": "/my-app/static/media/dai.5b15a8c6.jpg"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "76592d0e41fa020afd93",
+    "url": "/my-app/static/js/main.76592d0e.chunk.js"
+  },
+  {
+    "revision": "40b3e5a5562c8d7c16e2",
+    "url": "/my-app/static/js/1.40b3e5a5.chunk.js"
+  },
+  {
+    "revision": "76592d0e41fa020afd93",
+    "url": "/my-app/static/css/main.6e5cbe11.chunk.css"
+  },
+  {
+    "revision": "40b3e5a5562c8d7c16e2",
+    "url": "/my-app/static/css/1.0b958217.chunk.css"
+  },
+  {
+    "revision": "64be9758e5607fd288ca8ad5f973361a",
+    "url": "/my-app/index.html"
+  }
+];
