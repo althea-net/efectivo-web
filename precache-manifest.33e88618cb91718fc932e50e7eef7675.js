784ea52f1f1961b71c2faa1e3592f3e2e442b145
Jehan <jehan.tremback@gmail.com>
2019-02-20 13:29:38 -0800

    Updates

---
+++ precache-manifest.33e88618cb91718fc932e50e7eef7675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b15a8c6575231a6683cc8982faab6eb",
+    "url": "/static/media/dai.5b15a8c6.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9e3fac7a0e25c584d51e",
+    "url": "/static/js/main.9e3fac7a.chunk.js"
+  },
+  {
+    "revision": "bf099d22754421d37f11",
+    "url": "/static/js/1.bf099d22.chunk.js"
+  },
+  {
+    "revision": "bf099d22754421d37f11",
+    "url": "/static/css/1.666e7c71.chunk.css"
+  },
+  {
+    "revision": "33cde64f8b9ab3e1c6a0143a742bf5bb",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/efectivo-web/precache-manifest.fc60286b923ce708def217fdc1ace580.js"
+  "/precache-manifest.33e88618cb91718fc932e50e7eef7675.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/efectivo-web/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
